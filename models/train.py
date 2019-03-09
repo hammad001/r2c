1e2a5bb7a2e5c923c0324584127f78dd12d9672b
hammad001 <hammadabdullahayyubi@gmail.com>
2019-03-09 11:12:29 -0800

    Increased number of workers to 4 * GPUS if CPU >=32

--- models/train.py
+++ models/train.py
@@ -81,7 +81,7 @@ def _to_gpu(td):
                 non_blocking=True)
     return td
 
-num_workers = (4 * NUM_GPUS if NUM_CPUS == 32 else 2*NUM_GPUS)-1
+num_workers = (4 * NUM_GPUS if NUM_CPUS >= 32 else 2*NUM_GPUS)-1
 print(f"Using {num_workers} workers out of {NUM_CPUS} possible", flush=True)
 loader_params = {'batch_size': 96 // NUM_GPUS, 'num_gpus':NUM_GPUS, 'num_workers':num_workers}
 
