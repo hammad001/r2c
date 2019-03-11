d2833e803fae32a60e6f8c4fb4e6b08066d56441
TallnWide <mehrabtanjim@gmail.com>
2019-03-11 02:22:13 -0700

    soft attention

--- models/multiatt/model.py
+++ models/multiatt/model.py
@@ -414,19 +414,17 @@ class AttentionRA(Model):
 
             #print("shape", logits.size(), attended_q.size() )
 
-            #try:
-            if i == 0:
-                w_attended_q = logits[:,i].unsqueeze(1).expand_as(attended_q)*attended_q
-            else:
-                w_attended_q += logits[:,i].unsqueeze(1).expand_as(attended_q)*attended_q
-
-            
+            try:
+                if i == 0:
+                    w_attended_q = logits[:,i].unsqueeze(1).expand_as(attended_q)*attended_q
+                else:
+                    w_attended_q += logits[:,i].unsqueeze(1).expand_as(attended_q)*attended_q
                 
-            # except:
-            #     import pdb
-            #     pdb.set_trace()            
+            except:
+                import pdb
+                pdb.set_trace()            
         w_attended_q = w_attended_q.reshape(batch_size, 4, question_length, answer_length)
-        
+
         reasoning_inp = torch.cat([x for x, to_pool in [(a_rep, self.reasoning_use_answer),
                                                             (attended_o, self.reasoning_use_obj),
                                                             (w_attended_q, self.reasoning_use_question)]
